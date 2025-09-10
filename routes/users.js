5d5b89d711d1db50ec09a2d9ba96103ec3e9cb7e
Anca Maria Sirbu <ancamaria_sirbu@yahoo.com>
2025-09-10 12:55:26 +0200

    Auth (logout)

--- routes/users.js
+++ routes/users.js
@@ -34,4 +34,14 @@ router.post('/login', passport.authenticate('local', {failureFlash: true, failur
 
 })
 
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+}); 
+
 module.exports = router; 
