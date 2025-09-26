6cb0eb2d622c9653ffd0cda9dd071345f1764f8d
Anca Maria Sirbu <ancamaria_sirbu@yahoo.com>
2025-09-26 15:18:27 +0200

    Refactoring (reviews and users controller)

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,17 +7,11 @@ const Campground = require('../models/campground');
 
 
 router.get('/', catchAsync(campgrounds.index));
-
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
-
 router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
 router.get('/:id', catchAsync(campgrounds.showCampground));
-
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
 router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
 router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 
