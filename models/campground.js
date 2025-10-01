226581847a6be93b343e48182a60a6caa91a0202
Anca Maria Sirbu <ancamaria_sirbu@yahoo.com>
2025-10-01 12:45:58 +0200

    Delete imgs thumbnail card

--- models/campground.js
+++ models/campground.js
@@ -3,14 +3,22 @@ const Schema = mongoose.Schema;
 const Review = require('./review');
 
 
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_100');
+});
+
+ImageSchema.virtual("cardImage").get(function () {
+  return this.url.replace("/upload", "/upload/ar_4:3,c_crop");
+}); 
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
