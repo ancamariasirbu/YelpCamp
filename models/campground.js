aebb8950cf7aeed5ad82882c1febf05749df298a
Anca Maria Sirbu <ancamaria_sirbu@yahoo.com>
2025-10-06 11:57:40 +0200

    Cluster Map

--- models/campground.js
+++ models/campground.js
@@ -16,6 +16,8 @@ ImageSchema.virtual("cardImage").get(function () {
   return this.url.replace("/upload", "/upload/ar_4:3,c_crop");
 }); 
 
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
@@ -43,6 +45,12 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
 
 
